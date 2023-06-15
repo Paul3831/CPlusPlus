6746e0dd9780ff1e3378f04ad795d30bff40f332
Paul Moulerot <paul.moulerot@isitech.fr>
2023-06-15 16:40:49 +0200

    16h40

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -31,51 +31,62 @@ public:
 class Combat {
 private:
     std::string niveau;
+    Boxeur* coinBleu;
+    Boxeur* coinRouge;
 
 public:
-    Combat(std::string niveau) : niveau(niveau) {
+    Combat(std::string niveau) : niveau(niveau), coinBleu(nullptr), coinRouge(nullptr) {
 
     }
 
     ~Combat() {
-
+       
     }
 
     std::string getNiveau() const {
         return niveau;
+    }
+
+    void setCoinBleu(Boxeur* boxeur) {
+        coinBleu = boxeur;
+    }
+
+    Boxeur* getCoinBleu() const {
+        return coinBleu;
+    }
+
+    void setCoinRouge(Boxeur* boxeur) {
+        coinRouge = boxeur;
+    }
+
+    Boxeur* getCoinRouge() const {
+        return coinRouge;
     }
 };
 
 int main() {
     // Instanciation automatique d'un objet Boxeur
     Boxeur boxeur_1("Box_1", 75.0);
-
-    // Affichage des informations du boxeur_1
-    std::cout << "Adresse de boxeur_1 : " << &boxeur_1 << std::endl;
-    std::cout << "Nom de boxeur_1 : " << boxeur_1.getNom() << std::endl;
-    std::cout << "Poids de boxeur_1 : " << boxeur_1.getPoids() << " kg" << std::endl;
-
+    std::cout << "Boxeur 1 : " << &boxeur_1 << std::endl;
     // Instanciation dynamique d'un objet Boxeur
-    Boxeur* boxeur_2 = new Boxeur("Box_2", 78.0);
-
-    // Affichage des informations du boxeur_2
-    std::cout << "Adresse de boxeur_2 : " << boxeur_2 << std::endl;
-    std::cout << "Nom de boxeur_2 : " << boxeur_2->getNom() << std::endl;
-    std::cout << "Poids de boxeur_2 : " << boxeur_2->getPoids() << " kg" << std::endl;
-
-    // Modification du poids du boxeur_2
-    boxeur_2->setPoids(80.0);
-    std::cout << "Nouveau poids de boxeur_2 : " << boxeur_2->getPoids() << " kg" << std::endl;
-
-    // Libération de la mémoire du boxeur_2 (car instanciation dynamique)
-    delete boxeur_2;
+    Boxeur* boxeur_2 = new Boxeur("Box_2", 78.0); 
+    std::cout << "Boxeur 2 : " << boxeur_2 << std::endl;
 
     // Instanciation d'un objet Combat
     Combat combat_1("Comb_1_1/8");
 
-    // Affichage des informations du combat_1
-    std::cout << "Adresse de combat_1 : " << &combat_1 << std::endl;
-    std::cout << "Niveau de combat_1 : " << combat_1.getNiveau() << std::endl;
+    // Association entre combat_1 et boxeur_1
+    combat_1.setCoinBleu(&boxeur_1);
+
+    // Association entre combat_1 et boxeur_2
+    combat_1.setCoinRouge(boxeur_2);
+
+    // Vérification des associations
+    std::cout << "Boxeur du coin bleu : " << combat_1.getCoinBleu() << std::endl;
+    std::cout << "Boxeur du coin rouge : " << combat_1.getCoinRouge() << std::endl;
+
+    // Libération de la mémoire du boxeur_2 (car instanciation dynamique)
+    delete boxeur_2;
 
     return 0;
 }
